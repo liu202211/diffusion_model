8fd685c4c9d64b60608192b26d152068cecc4edc
fengliu <liu_git@163.com>
2022-12-06 18:13:28 +0800

    支持了大文件数据集的读入， 大文件数据集不需要完全读入内存

--- my_dataset.py
+++ my_dataset.py
@@ -5,16 +5,17 @@ from torch.utils.data import Dataset
 import cv2
 import torchvision
 
+SUPPORT_FILE_TYPE = ['.jpg', '.jpeg', '.bmp', '.gif', '.png']
 
-class My_dataset(Dataset):
-    SUPPORT_FILE_TYPE = ['.jpg', '.jpeg', '.bmp', '.gif']
+
+class Small_dataset(Dataset):
 
     def __init__(self, root):
         print(f'The dataset will being loaded from “{root}” in here, please waiting...')
         print(f'Do be checking the legitimacy of “{root}”...')
         assert os.path.isdir(root)
         print('checked success, to be going to load dataset...')
-        super(My_dataset, self).__init__()
+        super(Small_dataset, self).__init__()
 
         no_support_file = []  # 不支持的文件列表
         self.images = []
@@ -34,7 +35,7 @@ class My_dataset(Dataset):
 
             # 判断是不是支持的文件类型
             file_ext: str = os.path.splitext(file_path)[-1].lower()
-            if file_ext not in My_dataset.SUPPORT_FILE_TYPE:
+            if file_ext not in SUPPORT_FILE_TYPE:
                 no_support_file.append(file_path)
                 continue
 
@@ -61,7 +62,54 @@ class My_dataset(Dataset):
         return len(self.images)
 
 
+class Large_dataset(Dataset):
+    def __init__(self, root):
+        print(f'The dataset will being loaded from “{root}” in here, please waiting...')
+        print(f'Do be checking the legitimacy of “{root}”...')
+        assert os.path.isdir(root)
+        print('checked success, to be going to load dataset...')
+        no_support_file = []  # 不支持的文件列表
+        self.images = []
+
+        # 从文件夹中读取文件
+        print('reading files...')
+        file_paths = []
+        dir_list = os.listdir(root)
+        dir_len = len(dir_list)
+        for index, item in enumerate(dir_list):
+            p = int(index / dir_len * 100)
+            print('\r|' + '>' * p + '=' * (100 - p) + '|', end='')
+            file_path = os.path.join(root, item)
+            # 如果不是文件，就终止当前的读入
+            if not os.path.isfile(file_path):
+                continue
+
+            # 判断是不是支持的文件类型
+            file_ext: str = os.path.splitext(file_path)[-1].lower()
+            if file_ext not in SUPPORT_FILE_TYPE:
+                no_support_file.append(file_path)
+                continue
+
+            file_paths.append(file_path)
+        # file_paths = file_paths
+        available_img_len = len(file_paths)
+        print('\r' + ' ' * 110 + '\r', end='')
+        print(f'read success, the number of available image is {available_img_len}')
+        self.file_paths = file_paths
+        # # 读取支持的文件类型
+        print('dataset: success')
+
+    def __getitem__(self, item):
+        filename = self.file_paths[item]
+        img = cv2.imread(filename)
+        return torchvision.transforms.ToTensor()(img)
+
+    def __len__(self):
+        return len(self.file_paths)
+
+
 if __name__ == '__main__':
-    data = My_dataset('./faces')
+    data = Large_dataset('./dataset/00000')
+    # data = My_dataset('./faces')
     print(len(data))
     print(data[0].shape)
